3a3ba9d73b9c0d744ba38550f99b167eb3542440
reptation <david@musicalquilt.com>
2018-05-21 23:06:53 -0500

    another git alias/function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
-
 # C:\Users\$MYUSER\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 # for this to work, cd to above directory in admin shell and run "Set-ExecutionPolicy -Scope LocalMachine Unrestricted"
- Function gs { git.exe status }
+
+Function gs { git status }
+Function gpom { git push origin master }
