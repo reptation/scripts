c94ac8b47283b9eeb0ca0e031751edeab8a6ff73
reptation <david@musicalquilt.com>
2018-05-21 22:42:09 -0500

    Proof of concept 'alias' (aka function) for Powershell

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+
+# C:\Users\$MYUSER\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+# for this to work, cd to above directory in admin shell and run "Set-ExecutionPolicy -Scope LocalMachine Unrestricted"
+ Function gs { git.exe status }
