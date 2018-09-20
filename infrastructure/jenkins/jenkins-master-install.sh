566bddd70493081ef0bef7e2e12e66bfb8f747aa
reptation <david@musicalquilt.com>
2018-09-20 03:58:45 +0000

    install jenkins into docker container

---
+++ infrastructure/jenkins/jenkins-master-install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# from Jenkins documentation: https://jenkins.io/doc/book/installing/ 
+
+docker run -u root --rm -d -p 8080:8080 -p 50000:50000 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkinsci/blueocean 
