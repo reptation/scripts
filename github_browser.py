c01afa51c34d6136102e879ef893656468ee1b57
reptation <david@musicalquilt.com>
2018-09-21 04:40:10 +0000

    function get_url returns file data

--- github_browser.py
+++ github_browser.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python
 import requests
+import sys
 
-# example usage: prints out the script text
-requests.get(requests.get('https://api.github.com/repos/reptation/scripts/contents/.bash_aliases').json()['download_url']).text
+user=sys.argv[1]
+file_path=sys.argv[2]
 
-# todo turn into function that takes name of user, name of file path to return script text"
+def get_content_url(user, file_path):
+  first='https://api.github.com/repos/'
+  second=user
+  third="scripts/contents/"
+  fourth=file_path
+  the_url_seq=(first,second,third,fourth)
+  print(the_url_seq)
+  url="".join(the_url_seq)
+  print(url)
+  return(url)
+  # example usage: prints out the script text
+  #requests.get(requests.get('https://api.github.com/repos/reptation/scripts/contents/.bash_aliases').json()['download_url']).text
 
+  # todo turn into function that takes name of user, name of file path to return script text"
+
+
+get_content_url(user, file_path)
