2e29c1b3bc34955a4b7f328ab6bd5e6a6578dd28
reptation <david@musicalquilt.com>
2018-09-23 04:00:02 +0000

    remote-exec

---
+++ infrastructure/terraform/gcp/jenkins/jenkins-master.tf
@@ -0,0 +1,39 @@
+// Configure the Google Cloud provider
+provider "google" {
+ credentials = "${file("../account.json")}"
+  project     = "kube-farm-216603"
+  region      = "us-central1"
+}
+
+// Create a new instance
+resource "google_compute_instance" "default" {
+  name = "terraform-test-1"
+  machine_type = "n1-standard-1"
+  zone = "us-central1-a"
+
+  tags = ["darwin-corp", "sales"]
+  
+  boot_disk {
+    initialize_params {
+        image = "ubuntu-1804-lts"
+    }
+ }
+   
+  network_interface {
+    network = "default"
+  }
+
+  metadata {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+
+  provisioner "remote-exec" {
+    inline = [ "curl https://raw.githubusercontent.com/reptation/scripts/master/infrastructure/terraform/gcp/jenkins-install.sh | sudo bash" ]
+  }   
+}
