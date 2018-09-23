734a1447f709b1d8deee456d533d8759043cee1a
reptation <david@musicalquilt.com>
2018-09-23 02:45:13 +0000

    fixed headed

---
+++ infrastructure/terraform/gcp/jenkins/jenkins-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+git clone https://github.com/reptation/scripts.git
+sudo bash scripts/remote-setup.sh
+## TODO: install user accounts, keys
+
+sudo bash scripts/infrastructure/add-users.sh
+sudo bash scripts/infrastructure/docker/docker-install.sh
+## TODO: update install procedure 
+
+sudo bash scripts/infrastructure/docker/docker-compose-install.sh
+## TODO: update install procedure
+
+sudo bash scripts/infrastructure/jenkins/jenkins-master-install.sh
