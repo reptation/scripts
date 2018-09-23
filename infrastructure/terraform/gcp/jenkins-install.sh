6448ccf04a9eff5c329a3ae850a215cbaf1d6337
reptation <david@musicalquilt.com>
2018-09-23 02:05:56 +0000

    terraform to install jenkins on gcp vm

---
+++ infrastructure/terraform/gcp/jenkins-install.sh
@@ -0,0 +1,13 @@
+#bin/sh
+
+git clone https://github.com/reptation/scripts.git
+sudo bash scripts/remote-setup.sh
+## TODO: install user accounts, keys
+
+sudo bash scripts/infrastructure/docker/docker-install.sh
+## TODO: update install procedure 
+
+sudo bash scripts/infrastructure/docker/docker-compose-install.sh
+## TODO: update install procedure
+
+sudo bash scripts/infrastructure/jenkins/jenkins-master-install.sh
