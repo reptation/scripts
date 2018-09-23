f00cd8947817ea91259cb1c3384bd10d45cf0162
reptation <david@musicalquilt.com>
2018-09-23 03:57:07 +0000

    test jenkins-install.sh

--- infrastructure/terraform/gcp/jenkins/jenkins-install.sh
+++ infrastructure/terraform/gcp/jenkins/jenkins-install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 git clone https://github.com/reptation/scripts.git
-sudo bash scripts/remote-setup.sh
 ## TODO: install user accounts, keys
 
 sudo bash scripts/infrastructure/add-users.sh
@@ -12,3 +11,4 @@ sudo bash scripts/infrastructure/docker/docker-compose-install.sh
 ## TODO: update install procedure
 
 sudo bash scripts/infrastructure/jenkins/jenkins-master-install.sh
+sudo bash scripts/remote-setup.sh
