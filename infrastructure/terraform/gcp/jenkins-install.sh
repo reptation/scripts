7847f3d1cebf0109b3d588d24d7f6ab9a1fae242
reptation <david@musicalquilt.com>
2018-09-23 02:30:50 +0000

    add user install procedure

--- infrastructure/terraform/gcp/jenkins-install.sh
+++ infrastructure/terraform/gcp/jenkins-install.sh
@@ -4,6 +4,7 @@ git clone https://github.com/reptation/scripts.git
 sudo bash scripts/remote-setup.sh
 ## TODO: install user accounts, keys
 
+sudo bash scripts/infrastructure/add-users.sh
 sudo bash scripts/infrastructure/docker/docker-install.sh
 ## TODO: update install procedure 
 
