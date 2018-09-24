37f6055c873767e44ec6c069278602ae133da16a
reptation <david@musicalquilt.com>
2018-09-23 19:36:31 -0500

    add -y

--- github_browser.py
+++ github_browser.py
@@ -4,6 +4,9 @@ import sys
 
 user=sys.argv[1]
 file_path=sys.argv[2]
+first='https://api.github.com/repos/'
+second = user
+
 
 def get_content_url(user, file_path):
   first='https://api.github.com/repos/'
@@ -18,7 +21,18 @@ def get_content_url(user, file_path):
   # example usage: prints out the script text
   #requests.get(requests.get('https://api.github.com/repos/reptation/scripts/contents/.bash_aliases').json()['download_url']).text
 
-  # todo turn into function that takes name of user, name of file path to return script text"
+
+# TODO: turn into function that takes name of user, name of file path to return script text"
+def get_content(user,file_path)
+  pass
 
 
-get_content_url(user, file_path)
+def get_repos(user):
+  pass
+
+
+# TODO: create make_url method that takes 1 or more arguments are returns the joined product
+def make_url(*args):
+  return string.join(*args) #?
+
+
