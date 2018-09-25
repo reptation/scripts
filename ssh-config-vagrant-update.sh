9cddd73265d64888a80725499699262bf8d30962
reptation <david@musicalquilt.com>
2018-09-24 21:56:34 -0500

    one-liner to get identity file location

--- ssh-config-vagrant-update.sh
+++ ssh-config-vagrant-update.sh
@@ -2,19 +2,19 @@
 
 
 # TODO find way to save these to environment variables, perhaps with some post-processors?
-VAGRANT_PORT="2222"
-VAGRANT_IDENTITY_FILE="/long/path/name/to/key"
+VAGRANT_SSH_PORT="2200"
+VAGRANT_IDENTITY_FILE=$PWD$(find . | grep private | sed 's/\.\//\//')
 VAGRANT_HOSTNAME="127.0.0.1"
 VAGRANT_USER="vagrant"
 
+
 NEW_ENTRY="
-Host controlbox
+Host jenkins-master
         User "$VAGRANT_USER"
-	Port "$VAGRANT_PORT"
+	Port "$VAGRANT_SSH_PORT"
 	Hostname "$VAGRANT_HOSTNAME"
 	IdentityFile "$VAGRANT_IDENTITY_FILE" 
 "
-echo "$NEW_ENTRY"
 
+echo "$NEW_ENTRY" >> ~/.ssh/config
 
-# TODO append entry to ssh/config
