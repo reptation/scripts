85578cb08e47d3a83a890fa6e2ac629e06bb0d77
reptation <david@musicalquilt.com>
2018-10-26 04:26:57 +0000

    install hub github tool

---
+++ infrastructure/hub-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget https://github.com/github/hub/releases/download/v2.5.1/hub-linux-amd64-2.5.1.tgz
+tar -xvzf hub-linux-amd64-2.5.1.tgz
+cd hub-linux-amd64-2.5.1/
+sudo prefix=/usr/local ./install
+
