e01403471b544ed6801f5db5defd60ffac7f5ede
David Welch <welchdp@gmail.com>
2018-10-28 16:00:08 +0000

    add x perms, command completion

--- infrastructure/docker/docker-compose-install.sh
+++ infrastructure/docker/docker-compose-install.sh
@@ -2,3 +2,8 @@
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
+sudo chmod +x /usr/local/bin/docker-compose
+
+# command completion
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.22.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
