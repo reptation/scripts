03ea8efec9759cb2b191eee5cd2b0bae9a250094
reptation <david@musicalquilt.com>
2018-11-03 02:44:19 +0000

    add swap but do not modify fstab

--- swap-add.sh
+++ swap-add.sh
@@ -4,4 +4,4 @@
 sudo dd if=/dev/zero of=/swapfile bs=1M count=1024 oflag=append conv=notrunc
 sudo mkswap /swapfile
 sudo swapon /swapfile
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+#echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
