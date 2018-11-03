1c5be486803ac0c3f42b11fc79b0d387cde59066
reptation <david@musicalquilt.com>
2018-11-03 15:02:08 +0000

    add macros for script shebangs

--- .vimrc
+++ .vimrc
@@ -2,3 +2,6 @@ set number
 :set tabstop=4
 :set shiftwidth=4
 :set expandtab
+
+let @s='i#!/bin/sh'
+let @p='i#!/usr/bin/python3'
--- infrastructure/machine-learning/pytorch-install.sh
+++ infrastructure/machine-learning/pytorch-install.sh
@@ -3,4 +3,5 @@
 sudo apt-get install python3-tk
 sudo pip3 install -r requirements.txt
 # download faces data 
-#wget https://download.pytorch.org/tutorial/faces.zip
+# wget https://download.pytorch.org/tutorial/faces.zip
+# unzip faces.zip
