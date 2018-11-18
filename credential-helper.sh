584e37b901e37226b9c2199de5b553df7e3a4630
reptation <david@musicalquilt.com>
2018-11-18 18:28:32 +0000

    working through rabbitmq tutorials

--- credential-helper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# https://alanedwardes.com/blog/posts/git-username-password-environment-variables/
-
-echo username=$GIT_USERNAME
-echo password=$GIT_PASSWORD
-
-# git config credential.helper "/bin/bash ./credential-helper.sh"
