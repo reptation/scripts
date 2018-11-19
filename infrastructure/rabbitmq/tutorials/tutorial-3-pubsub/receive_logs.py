2c5caf68b1cbb3375e961171c059068cd41242b0
reptation <david@musicalquilt.com>
2018-11-19 03:32:13 +0000

    receive logs

--- infrastructure/rabbitmq/tutorials/tutorial-3-pubsub/receive_logs.py
+++ infrastructure/rabbitmq/tutorials/tutorial-3-pubsub/receive_logs.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python3
-
-
+#!/usr/bin/env python
 import pika
-import sys
-
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
@@ -11,9 +7,19 @@ channel = connection.channel()
 channel.exchange_declare(exchange='logs',
                          exchange_type='fanout')
 
-message = ' '.join(sys.argv[1:]) or "info: Hello World!"
-channel.basic_publish(exchange='logs',
-                      routing_key='',
-                      body=message)
-print(" [x] Sent %r" % message)
-connection.close()
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='logs',
+                   queue=queue_name)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] %r" % body)
+
+channel.basic_consume(callback,
+                      queue=queue_name,
+                      no_ack=True)
+
+channel.start_consuming()
